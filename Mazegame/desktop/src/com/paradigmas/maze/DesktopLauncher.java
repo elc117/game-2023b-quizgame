dc0e48abc927705e1cb1d5c830035587dae632b0
Arthur <88109336+arthurmfroes@users.noreply.github.com>
2023-11-20 21:00:07 -0300

    teste de movimentação e mapa BÁSICO
    todolist criada

---
+++ Mazegame/desktop/src/com/paradigmas/maze/DesktopLauncher.java
@@ -0,0 +1,18 @@
+package com.paradigmas.maze;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.paradigmas.maze.MazeGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setWindowedMode(800, 600);
+		config.setResizable(false);
+
+		config.setForegroundFPS(30);
+		config.setTitle("Mazegame");
+		new Lwjgl3Application(new MazeGame(), config);
+	}
+}
